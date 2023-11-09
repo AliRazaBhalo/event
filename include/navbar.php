d28033df26ba615fd368d0737a7857bb998f4969
AliRazaBhalo <120156700+AliRazaBhalo@users.noreply.github.com>
2023-11-09 16:25:11 +0500

    files

---
+++ include/navbar.php
@@ -0,0 +1,66 @@
+<header class="main-header transparent">
+<div class="container">
+<div class="navigation-wrapper">
+<div class="navigation-inner">
+<div class="site-logo">
+<a href="index.html"><img src="assets/img/logo-dark.png" alt="Softgen"></a>
+</div>
+<nav class="navigation-menu">
+<ul class="main-menu">
+<li><a href="index.php">Home</a>
+
+
+</li>
+<li><a href="about.php">About</a></li>
+<li><a href="team.php">Team</a></li>
+<li><a href="about.html">Our Company</a>
+<ul>
+<li><a href="about.html">About Us</a></li>
+<li><a href="services.html">Our Services</a></li>
+<li><a href="service-details.html">Services Details</a></li>
+<li><a href="team.html">Our Team</a></li>
+<li><a href="team-details.html">Team Details</a></li>
+<li><a href="pricing.html">Plans &amp; Pricing</a></li>
+<li><a href="testimonial.html">Testimonials</a></li>
+<li><a href="faqs.html">Help &amp; FAQ's</a></li>
+<li><a href="404.html">404 Error</a></li>
+</ul>
+</li>
+<li><a href="projects.html">Projects</a>
+<ul>
+<li><a href="projects.html">Our Projects</a></li>
+<li><a href="project-details.html">Project Details</a></li>
+</ul>
+</li>
+<!-- <li><a href="blog-grid.html">Blog</a>
+<ul>
+<li><a href="blog-grid.html">Blog Grid</a></li>
+<li><a href="blog-classic.html">Blog Classic</a></li>
+<li><a href="blog-details.html">Blog Details</a></li>
+</ul>
+</li> -->
+<li><a href="contact.php">Contact</a></li>
+</ul>
+</nav>
+<div class="header-right">
+<a href="contact.html" class="default-btn d-none d-md-block">Start Free Trial</a>
+<button class="sidebar-trigger open">
+<svg class="trigger-opener" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="12.7px" viewBox="0 0 16 12.7" style="enable-background:new 0 0 16 12.7;" xml:space="preserve">
+<g>
+<rect x="5" y="8.4" width="11" height="2" />
+<rect x="0" y="2.4" width="16" height="2" />
+</g>
+</svg>
+</button>
+<div class="mobile-menu-icon">
+<div class="burger-menu">
+<div class="line-menu line-half first-line"></div>
+<div class="line-menu"></div>
+<div class="line-menu line-half last-line"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</header>
---
+++ team.php
@@ -0,0 +1,359 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
+<!-- Mirrored from html.dynamiclayers.net/te/softgen/team.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 02 Nov 2023 13:25:49 GMT -->
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Softgen Software and Technology HTML5 Template">
+<meta name="author" content="ThemeEaster">
+<title>Softgen | Software &amp; Technology HTML5 Template</title>
+<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/animate.min.css">
+<link rel="stylesheet" href="assets/css/keyframe-animation.css">
+<link rel="stylesheet" href="assets/css/line-awesome.min.css">
+<link rel="stylesheet" href="assets/css/odometer.min.css">
+<link rel="stylesheet" href="assets/css/swiper.min.css">
+<link rel="stylesheet" href="assets/css/venobox.min.css">
+<link rel="stylesheet" href="assets/css/animated-text.min.css">
+<link rel="stylesheet" href="assets/css/common-style.css">
+<link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+<!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+<div class="site-preloader">
+<div class="spinner"></div>
+</div>
+
+
+<?php  include('include/navbar.php'); ?>
+
+<div id="sidebar-area" class="sidebar-area">
+<button class="sidebar-trigger close">
+<svg class="sidebar-close" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="12.7px" viewBox="0 0 16 12.7" style="enable-background:new 0 0 16 12.7;" xml:space="preserve">
+<g>
+<rect x="0" y="5.4" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -2.1569 7.5208)" width="16" height="2"></rect>
+<rect x="0" y="5.4" transform="matrix(0.7071 0.7071 -0.7071 0.7071 6.8431 -3.7929)" width="16" height="2"></rect>
+</g>
+</svg>
+</button>
+<div class="sidebar-content">
+<div class="site-logo">
+<a href="index.html"><img src="assets/img/logo-dark.png" alt="logo"></a>
+</div>
+<p>9621 Agnes Crossing, Lake Suzanneview, New Mexico Island 84604-9295.</p>
+<ul class="sidebar-info">
+<li><span>Call:</span> 5267-214-392</li>
+<li><span>Email:</span> <a href="https://html.dynamiclayers.net/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="a3eacdc5cc8defc6c2c7c5cfccd4e3cec2cacf8dc0ccce">[email&#160;protected]</a></li>
+</ul>
+<ul class="sidebar-social">
+<li><a href="#"><i class="lab la-facebook-f"></i></a></li>
+<li><a href="#"><i class="lab la-twitter"></i></a></li>
+<li><a href="#"><i class="lab la-youtube"></i></a></li>
+<li><a href="#"><i class="lab la-behance"></i></a></li>
+</ul>
+</div>
+</div>
+
+
+<div id="sidebar-overlay"></div>
+<section class="page-header bd-bottom">
+<div class="lines">
+<div class="line"></div>
+<div class="line"></div>
+<div class="line"></div>
+<div class="line"></div>
+</div>
+<div class="container">
+<div class="page-header-info text-left">
+<h4>Our team</h4>
+<h2>Our Team Create Great <br>Things <span class="wow border-animation" data-wow-delay="300ms">Together!</span></h2>
+<p>Our versatile team is built of designers, developers and <br>digital marketers who all bring unique experience.</p>
+</div>
+</div>
+<div class="animated-dots">
+<span class="dot lf-left-right"></span>
+<span class="dot lf-up-down"></span>
+<span class="dot lf-up-down"></span>
+<span class="dot lf-rotate-center"></span>
+<span class="dot lf-left-right"></span>
+<span class="dot lf-rotate-center"></span>
+</div>
+</section>
+
+<section class="team-section bg-grey padding">
+<div class="container">
+<div class="row">
+<div class="col-lg-3 col-sm-6 padding-15">
+<div class="team-item">
+<div class="team-thumb">
+<img src="assets/img/team-shape.png" alt="img">
+<img src="assets/img/person01.jpg" alt="img">
+</div>
+<div class="team-content">
+<h4>Corportate</h4>
+<h3><a href="team-details.html">Félix Lengyel</a></h3>
+<ul class="team-social">
+<li><a href="#">Fb</a></li>
+<li><a href="#">Tw</a></li>
+<li><a href="#">Yt</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-sm-6 padding-15">
+<div class="team-item">
+<div class="team-thumb">
+<img src="assets/img/team-shape.png" alt="img">
+<img src="assets/img/person02.jpg" alt="img">
+</div>
+<div class="team-content">
+<h4>Marketing</h4>
+<h3><a href="team-details.html">Timothy Paradis</a></h3>
+<ul class="team-social">
+<li><a href="#">Fb</a></li>
+<li><a href="#">Tw</a></li>
+<li><a href="#">Yt</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-sm-6 padding-15">
+<div class="team-item">
+<div class="team-thumb">
+<img src="assets/img/team-shape.png" alt="img">
+<img src="assets/img/person03.jpg" alt="img">
+</div>
+<div class="team-content">
+<h4>Finacial</h4>
+<h3><a href="team-details.html">Stephen Miller</a></h3>
+<ul class="team-social">
+<li><a href="#">Fb</a></li>
+<li><a href="#">Tw</a></li>
+<li><a href="#">Yt</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-sm-6 padding-15">
+<div class="team-item">
+<div class="team-thumb">
+<img src="assets/img/team-shape.png" alt="img">
+<img src="assets/img/person04.jpg" alt="img">
+</div>
+<div class="team-content">
+<h4>Management</h4>
+<h3><a href="team-details.html">Kristin Castillo</a></h3>
+<ul class="team-social">
+<li><a href="#">Fb</a></li>
+<li><a href="#">Tw</a></li>
+<li><a href="#">Yt</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-sm-6 padding-15">
+<div class="team-item">
+<div class="team-thumb">
+<img src="assets/img/team-shape.png" alt="img">
+<img src="assets/img/person05.jpg" alt="img">
+</div>
+<div class="team-content">
+<h4>Corportate</h4>
+<h3><a href="team-details.html">Félix Lengyel</a></h3>
+<ul class="team-social">
+<li><a href="#">Fb</a></li>
+<li><a href="#">Tw</a></li>
+<li><a href="#">Yt</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-sm-6 padding-15">
+<div class="team-item">
+<div class="team-thumb">
+<img src="assets/img/team-shape.png" alt="img">
+<img src="assets/img/person06.jpg" alt="img">
+</div>
+<div class="team-content">
+<h4>Marketing</h4>
+<h3><a href="team-details.html">Timothy Paradis</a></h3>
+<ul class="team-social">
+<li><a href="#">Fb</a></li>
+<li><a href="#">Tw</a></li>
+<li><a href="#">Yt</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-sm-6 padding-15">
+<div class="team-item">
+<div class="team-thumb">
+<img src="assets/img/team-shape.png" alt="img">
+<img src="assets/img/person07.jpg" alt="img">
+</div>
+<div class="team-content">
+<h4>Finacial</h4>
+<h3><a href="team-details.html">Stephen Miller</a></h3>
+<ul class="team-social">
+<li><a href="#">Fb</a></li>
+<li><a href="#">Tw</a></li>
+<li><a href="#">Yt</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-sm-6 padding-15">
+<div class="team-item">
+<div class="team-thumb">
+<img src="assets/img/team-shape.png" alt="img">
+<img src="assets/img/person08.jpg" alt="img">
+</div>
+<div class="team-content">
+<h4>Management</h4>
+<h3><a href="team-details.html">Kristin Castillo</a></h3>
+<ul class="team-social">
+<li><a href="#">Fb</a></li>
+<li><a href="#">Tw</a></li>
+<li><a href="#">Yt</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<section class="cta-section">
+<img class="anim-illustration parallax-item" src="assets/img/element-4.png" alt="illustration">
+<img class="anim-illustration parallax-item bottom" src="assets/img/element-5.png" alt="illustration">
+<div class="container">
+<div class="cta-wrap">
+<div class="cta-content">
+<h2>Ready to Improve your <br> customer retention rate?</h2>
+<p>Our success in creating business solutions is due in large part spacially <br>to talented and highly committed team.</p>
+<div class="btn-group">
+<a href="contact.html" class="default-btn">Get started for free</a>
+<a class="play-btn venobox" data-vbtype="video" href="https://www.youtube.com/watch?v=ZLaG1migE00">
+<span class="play-icon"><svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+<path fill="currentColor" d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path>
+</svg></span>
+</a>
+</div>
+</div>
+<div class="animated-dots">
+<span class="dot lf-left-right"></span>
+<span class="dot lf-up-down"></span>
+<span class="dot lf-up-down"></span>
+<span class="dot lf-rotate-center"></span>
+<span class="dot lf-left-right"></span>
+<span class="dot lf-rotate-center"></span>
+</div>
+</div>
+</div>
+</section>
+
+<section class="blog-section padding">
+<div class="container">
+<div class="section-heading text-center mb-40">
+<h2>News and <span class="wow border-animation" data-wow-delay="300ms">Insights.</span></h2>
+<p>Our success in creating business solutions is due in large part <br> spacially to talented and highly committed team.</p>
+</div>
+<div class="row grid-post">
+<div class="col-lg-4 col-md-6 padding-15">
+<div class="post-card">
+<div class="post-thumb">
+<img src="assets/img/post-1.jpg" alt="post">
+<a href="blog-grid.html" class="post-category">Business</a>
+</div>
+<div class="post-content-wrap">
+<ul class="post-meta">
+<li><i class="las la-calendar"></i>Jan 01 2024</li>
+<li><i class="las la-user"></i>Elliot Alderson</li>
+</ul>
+<div class="post-content">
+<h3><a href="blog-details.html" class="hover">How to start initian startup business.</a></h3>
+<p>Financial experts support or help you to to find out which way you can raise your funds more...</p>
+<a href="blog-details.html" class="read-more"><span>Read More</span><i class="las la-long-arrow-alt-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 padding-15">
+<div class="post-card">
+<div class="post-thumb">
+<img src="assets/img/post-2.jpg" alt="post">
+<a href="blog-grid.html" class="post-category">Startup</a>
+</div>
+<div class="post-content-wrap">
+<ul class="post-meta">
+<li><i class="las la-calendar"></i>Jan 01 2024</li>
+<li><i class="las la-user"></i>Elliot Alderson</li>
+</ul>
+<div class="post-content">
+<h3><a href="blog-details.html">Principle efective marketing.</a></h3>
+<p>Financial experts support or help you to to find out which way you can raise your funds more...</p>
+<a href="blog-details.html" class="read-more"><span>Read More</span><i class="las la-long-arrow-alt-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 padding-15">
+<div class="post-card">
+<div class="post-thumb">
+<img src="assets/img/post-3.jpg" alt="post">
+<a href="blog-grid.html" class="post-category">Finance</a>
+</div>
+<div class="post-content-wrap">
+<ul class="post-meta">
+<li><i class="las la-calendar"></i>Jan 01 2024</li>
+<li><i class="las la-user"></i>Elliot Alderson</li>
+</ul>
+<div class="post-content">
+<h3><a href="blog-details.html">Innovative business finance solution.</a></h3>
+<p>Financial experts support or help you to to find out which way you can raise your funds more...</p>
+<a href="blog-details.html" class="read-more"><span>Read More</span><i class="las la-long-arrow-alt-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<?php  include('include/footer.php'); ?>
+
+<div id="scrollup">
+<button id="scroll-top" class="scroll-to-top"><i class="las la-arrow-up"></i></button>
+</div>
+
+
+<script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/js/vendor/jquary-3.6.0.min.js"></script>
+<script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+<script src="assets/js/vendor/atropos.min.js"></script>
+<script src="assets/js/vendor/bootstrap.min.js"></script>
+<script src="assets/js/vendor/popper.min.js"></script>
+<script src="assets/js/vendor/odometer.min.js"></script>
+<script src="assets/js/vendor/waypoints.min.js"></script>
+<script src="assets/js/vendor/venobox.min.js"></script>
+<script src="assets/js/vendor/swiper.min.js"></script>
+<script src="assets/js/vendor/smooth-scroll.js"></script>
+<script src="assets/js/vendor/wow.min.js"></script>
+<script src="assets/js/vendor/animated-text.min.js"></script>
+<script src="assets/js/vendor/gsap.min.js"></script>
+<script src="assets/js/vendor/scroll-trigger.min.js"></script>
+<script src="assets/js/vendor/parallax-scroll.min.js"></script>
+<script src="assets/js/mailchimp.js"></script>
+<script src="assets/js/main.js"></script>
+<script>(function(){var js = "window['__CF$cv$params']={r:'81fcaafbdcca87b7',t:'MTY5ODkzMTE1My40ODIwMDA='};_cpo=document.createElement('script');_cpo.nonce='',_cpo.src='../../cdn-cgi/challenge-platform/h/b/scripts/jsd/61b90d1d/main.js',document.getElementsByTagName('head')[0].appendChild(_cpo);";var _0xh = document.createElement('iframe');_0xh.height = 1;_0xh.width = 1;_0xh.style.position = 'absolute';_0xh.style.top = 0;_0xh.style.left = 0;_0xh.style.border = 'none';_0xh.style.visibility = 'hidden';document.body.appendChild(_0xh);function handler() {var _0xi = _0xh.contentDocument || _0xh.contentWindow.document;if (_0xi) {var _0xj = _0xi.createElement('script');_0xj.innerHTML = js;_0xi.getElementsByTagName('head')[0].appendChild(_0xj);}}if (document.readyState !== 'loading') {handler();} else if (window.addEventListener) {document.addEventListener('DOMContentLoaded', handler);} else {var prev = document.onreadystatechange || function () {};document.onreadystatechange = function (e) {prev(e);if (document.readyState !== 'loading') {document.onreadystatechange = prev;handler();}};}})();</script></body>
+
+<!-- Mirrored from html.dynamiclayers.net/te/softgen/team.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 02 Nov 2023 13:25:49 GMT -->
+</html>
